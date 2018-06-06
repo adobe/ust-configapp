5f3a0647731e2b5096871d04c5c0ab0389d68b16
bajwa-adobe <bajwa@adobe.com>
2018-06-06 17:16:15 +0100

    Added and configured code-signing cert

---
+++ cert/generate_pfx.bat
@@ -0,0 +1,6 @@
+REM "Running UST in TEST-MODE"
+cd %~dp0
+openssl genrsa 2048 > private.pem
+openssl req -x509 -new -key private.pem -out public.pem
+openssl pkcs12 -export -in public.pem -inkey private.pem -out codecert.pfx
+pause
